cab6b2897d745a897414168c4b36ae1700102917
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-07-30 02:10:17 +0300

    feat: some structure

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+
+function App() {
+    return (
+        <div className='app'>
+            <Header>
+
+            </Header>
+            <Main>
+
+            </Main>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
