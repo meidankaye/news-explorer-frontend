777d5d550a44a6cea21896264742128e9b4c4d97
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-08-02 17:03:50 +0300

    feat: main and about

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,21 @@
 import "./Main.css";
+import SearchForm from "../SearchForm/SearchForm";
+import About from "../About/About";
 
 function Main() {
-
+  return (
+    <section className="main">
+      <div className="main__content">
+        <h1 className="main__title">What's going on in the world?</h1>
+        <p className="main__subtitle">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+        <SearchForm />
+      </div>
+      <About />
+    </section>
+  );
 }
 
 export default Main;
