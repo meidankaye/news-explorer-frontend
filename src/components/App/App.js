90c76849e513ee3537f4d341f63c0d293453f76f
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-08-04 23:03:34 +0300

    feat: savednews header init

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
 import HeaderButton from "../HeaderButton/HeaderButton";
 import Navigation from "../Navigation/Navigation";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import SavedNewsHeader from "../SavedNewsHeader/SavedNewsHeader";
 
 function App() {
   return (
@@ -13,7 +15,10 @@ function App() {
           <HeaderButton />
         </Navigation>
       </Header>
-      <Main></Main>
+      <Routes>
+        <Route index path="/" element={<Main />} />
+        <Route path="/saved-news" element={<SavedNewsHeader />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -0,0 +1,20 @@
+import "./SavedNewsHeader.css";
+
+function SavedNewsHeader() {
+  return (
+    <section className="saved-news-header">
+      <span className="saved-news-header__subtitle">Saved articles</span>
+      <h2 className="saved-news-header__title">
+        Elise, you have 5 saved articles
+      </h2>
+      <p className="saved-news-header__text">
+        By keywords:{" "}
+        <span className="saved-news-header__text_bold">
+          Nature, Yellowstone and 2 other
+        </span>
+      </p>
+    </section>
+  );
+}
+
+export default SavedNewsHeader;
