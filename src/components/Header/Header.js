8848ade57ce2d29bf357c93102a0a8702f4f021b
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-08-08 17:26:22 +0300

    feat: styling for main tablet and phone

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import "./Header.css";
-// import menuWhite from "../../images/menu-white.svg";
+import menuWhite from "../../images/menu-white.svg";
 
 function Header({ children }) {
   return (
     <header className="header">
       <span className="header__logo">NewsExplorer</span>
-      {children}
-      {/* <img src={menuWhite} className="header__menu" alt="Menu icon" /> */}
+      {/* {children} */}
+      <img src={menuWhite} className="header__menu" alt="Menu icon" />
     </header>
   );
 }
